3cbe22211cd55487dd24baa8f3dbf3caa8f069d4
Kazeem Dosunmu <97337763+thedoskit@users.noreply.github.com>
2023-03-07 22:51:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -104,8 +104,10 @@ const connect = function () {
         const json = await response.json()
 
         // Redirecting the user to the download URL
+        console.log('json:', json);
         window.location.href = json.url
       } catch (e) {
+        console.error('Error:', response.statusText);
         alert("Incorrect download URL.")
       }
     } else {
